5f6efc369aa354c03e116d5c6152b7d2c8ddb15f
Ansh Mehta <8anshmehta@gmail.com>
2021-07-21 22:17:12 +0530

    queries shifted to DynamoDB

--- server.js
+++ server.js
@@ -4,261 +4,416 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 const moment = require('moment');
 const nodemailer = require('nodemailer');
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+
+const usersTable = { TableName: 'users' };
+const transactionsTable = { TableName: 'transactions' };
+
+const config = require('./config');
+var docClient;
+
+try {
+	AWS.config.update(config);
+	docClient = new AWS.DynamoDB.DocumentClient();
+	console.log('Connected to DynamoDB!');
+} catch (err) {
+	console.log('Error in starting DB.');
+}
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const MongoClient = require('mongodb').MongoClient;
-MongoClient.connect(
-	`mongodb+srv://anshm:NcuAQEwPpeA6bgRe@salt-testapp.334w6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-	{ useUnifiedTopology: true }
-).then(client => {
-	console.log('Connected to Database');
+app.post('/create_user', async (req, res, next) => {
+	if (!req.body.email || !req.body.balance) {
+		res.status(400).json({ message: 'Parameters missing' });
+		return;
+	}
 
-	const db = client.db('SALT-test');
-	const usersCollection = db.collection('users');
-	const transactionsCollection = db.collection('transactions');
+	var { email, balance } = req.body;
 
-	app.post('/create_user', async (req, res, next) => {
-		if (!req.body.email || !req.body.balance) {
-			res.status(400).json({ message: 'Parameters missing' });
-			return;
-		}
-
-		var { email, balance } = req.body;
-
-		await usersCollection.findOne({ email }).then(async resp => {
-			if (resp !== undefined) {
-				res.status(400).json({ message: 'User already exists!' });
-				return;
-			}
-			var id = Math.random().toFixed(16).split('.')[1];
-
-			const newUser = await usersCollection.insertOne({
-				email,
-				id,
-				balance: Number(balance),
-				password: '1234',
-			});
-
-			if (!newUser) {
+	await docClient.get(
+		{
+			...usersTable,
+			Key: { email },
+		},
+		async (err, data) => {
+			if (err) {
 				res.status(500).json({ message: 'An error occured.' });
 				return;
 			}
+			if (Object.keys(data).length > 0) {
+				res.status(400).json({ message: 'User already exists!' });
+				return;
+			}
 
-			var transporter = nodemailer.createTransport({
-				service: 'gmail',
-				auth: {
-					user: 'asquaremrocks@gmail.com',
-					pass: 'rvsuljwlvazzaxnm',
-				},
-			});
-
-			const mailOptions = {
-				from: 'asquaremrocks@gmail.com', // sender address
-				to: email, // list of receivers
-				subject: 'New user added to MiniBank!', // Subject line
-				html: `<h1>Welcome to Mini Bank!</h1><h3>Here are your login credentials:</h3><p>Email: <b>${email}</b></p><p>Password: <b>${'1234'}</b></p><p>Note: This is just for testing purposes.</p>`, // plain text body
+			const newUser = {
+				id: Math.random().toFixed(16).split('.')[1],
+				email,
+				password: '1234',
+				balance: Number(balance),
 			};
-			transporter.sendMail(mailOptions, function (err, info) {
+
+			await docClient.put({ ...usersTable, Item: newUser }, (err, data) => {
 				if (err) {
-					res.status(500).json({ message: 'An error occured during sending mail.' });
+					res.status(500).json({ message: 'An error occured.' });
 					return;
-				} else console.log(info);
+				}
+				var transporter = nodemailer.createTransport({
+					service: 'gmail',
+					auth: {
+						user: 'asquaremrocks@gmail.com',
+						pass: 'rvsuljwlvazzaxnm',
+					},
+				});
+
+				const mailOptions = {
+					from: 'asquaremrocks@gmail.com', // sender address
+					to: email, // list of receivers
+					subject: 'New user added to MiniBank!', // Subject line
+					html: `<h1>Welcome to Mini Bank!</h1><h3>Here are your login credentials:</h3><p>Email: <b>${email}</b></p><p>Password: <b>${'1234'}</b></p><p>Note: This is just for testing purposes.</p>`, // plain text body
+				};
+				transporter.sendMail(mailOptions, function (err, info) {
+					if (err) {
+						res.status(500).json({ message: 'An error occured during sending mail.' });
+						return;
+					}
+				});
+				res.status(201).json({ message: 'User created successfully!' });
 			});
+		}
+	);
+});
 
-			res.status(201).json({ message: 'User created successfully!' });
-		});
-	});
+app.post('/verify', async (req, res, next) => {
+	if (!req.body.email || !req.body.password) {
+		res.status(400).json({ message: 'Parameters missing' });
+		return;
+	}
 
-	app.post('/verify', async (req, res, next) => {
-		if (!req.body.email || !req.body.password) {
-			res.status(400).json({ message: 'Parameters missing' });
+	var { email, password } = req.body;
+
+	await docClient.get(
+		{
+			...usersTable,
+			Key: { email },
+		},
+		(err, data) => {
+			if (err) {
+				res.status(500).json({ message: 'An error occured.' });
+				return;
+			}
+			if (Object.keys(data).length === 0) {
+				res.status(404).json({ message: 'No user found.' });
+				return;
+			}
+			const user = data.Item;
+			if (password !== user.password) {
+				res.status(401).json({ message: 'Incorrect password.' });
+				return;
+			}
+			res.status(200).json({ balance: user.balance, id: user.id, message: 'User verified successfully!' });
+		}
+	);
+});
+
+app.post('/transfer', async (req, res, next) => {
+	if (!req.body.sender_email || !req.body.receiver_email || !req.body.amt) {
+		res.status(400).json({ message: 'Parameters missing' });
+		return;
+	}
+
+	var { sender_email, receiver_email, amt } = req.body;
+	var sender_bal, receiver_bal, sender_id, receiver_id;
+
+	amt = Number(amt);
+
+	await docClient.get({ ...usersTable, Key: { email: sender_email } }, async (err, data) => {
+		if (err) {
+			res.status(500).json({ message: 'An error occured.' });
 			return;
 		}
-
-		var { email, password } = req.body;
-
-		const user = await usersCollection.findOne({ email });
-
-		if (user === null) {
-			res.status(404).json({ message: 'No user found.' });
-			return;
-		}
-
-		if (password !== user.password) {
-			res.status(401).json({ message: 'Incorrect password.' });
-			return;
-		}
-
-		res.status(201).json({ balance: user.balance, id: user.id, message: 'User verified successfully!' });
-	});
-
-	app.post('/transfer', async (req, res, next) => {
-		if (!req.body.sender || !req.body.receiver || !req.body.amt) {
-			res.status(400).json({ message: 'Parameters missing' });
-			return;
-		}
-
-		var { sender, receiver, amt } = req.body;
-
-		const from = await usersCollection.findOne({ id: sender });
-
-		if (!from) {
+		if (Object.keys(data).length === 0) {
 			res.status(404).json({ message: 'Sender not found.' });
 			return;
 		}
 
-		if (from.balance < amt || from.balance === 0) {
+		sender_bal = data.Item.balance;
+		sender_id = data.Item.id;
+
+		if (sender_bal < amt || sender_bal === 0) {
 			res.status(401).json({ message: 'Insufficient balance.' });
 			return;
 		}
 
-		const to = await usersCollection.findOne({ id: receiver });
-
-		if (!to) {
-			res.status(404).json({ message: 'Receiver not found.' });
-			return;
-		}
-
-		await usersCollection
-			.findOneAndUpdate({ id: sender }, { $inc: { balance: -Number(amt) } })
-			.then(async (err, doc) => {
-				await usersCollection
-					.findOneAndUpdate({ id: receiver }, { $inc: { balance: Number(amt) } })
-					.then(async err => {
-						await transactionsCollection
-							.insertOne({
-								sender_id: sender,
-								receiver_id: receiver,
-								amt: Number(amt),
-								timestamp: moment().format(),
-							})
-							.then(() => {
-								res.status(201).json({ message: 'transferred!' });
-							});
-					});
-			});
-	});
-
-	app.post('/transact', async (req, res, next) => {
-		//type, amt (debit=0, credit=1)
-		if (!req.body.id || !req.body.type || !req.body.amt) {
-			res.status(400).json({ message: 'Parameters missing' });
-			return;
-		}
-
-		var { id, type, amt } = req.body;
-
-		const account = await usersCollection.findOne({ id });
-
-		if (!account) {
-			res.status(404).json({ message: 'Account not found.' });
-			return;
-		}
-
-		async function debit() {
-			if (account.balance < amt || account.balance === 0) {
-				res.status(401).json({ message: 'Insufficient balance.' });
+		await docClient.get({ ...usersTable, Key: { email: receiver_email } }, async (err, data) => {
+			if (err) {
+				res.status(500).json({ message: 'An error occured.' });
 				return;
 			}
-			await usersCollection.findOneAndUpdate({ id }, { $inc: { balance: -Number(amt) } }).then(async () => {
-				await transactionsCollection
-					.insertOne({
-						sender_id: id,
-						receiver_id: 100,
-						amt: Number(amt),
-						timestamp: moment().format(),
-					})
-					.then(() => res.status(201).json({ message: 'Amount debited!' }));
-			});
-		}
+			if (Object.keys(data).length === 0) {
+				res.status(404).json({ message: 'Receiver not found.' });
+				return;
+			}
+			receiver_bal = data.Item.balance;
+			receiver_id = data.Item.id;
 
-		async function credit() {
-			await usersCollection.findOneAndUpdate({ id }, { $inc: { balance: Number(amt) } }).then(async () => {
-				await transactionsCollection
-					.insertOne({
-						sender_id: 100,
-						receiver_id: id,
-						amt: Number(amt),
-						timestamp: moment().format(),
-					})
-					.then(() => res.status(201).json({ message: 'Amount credited!' }));
-			});
-		}
+			sender_bal -= amt;
+			receiver_bal += amt;
 
-		Number(type) === 1 ? credit() : debit();
+			await docClient.update(
+				{
+					...usersTable,
+					Key: { email: sender_email },
+					UpdateExpression: 'set balance = :r',
+					ExpressionAttributeValues: { ':r': Number(sender_bal) },
+				},
+				async err => {
+					if (err) {
+						res.status(500).json({ message: 'An error occured.' });
+						return;
+					}
+					await docClient.update(
+						{
+							...usersTable,
+							Key: { email: receiver_email },
+							UpdateExpression: 'set balance=:r',
+							ExpressionAttributeValues: { ':r': receiver_bal },
+						},
+						async err => {
+							if (err) {
+								console.log(err);
+								res.status(500).json({ message: 'An error occured.' });
+								return;
+							}
+							const newTransaction = {
+								transaction_id: uuidv4(),
+								sender_id,
+								receiver_id,
+								amt,
+								timestamp: moment().format(),
+							};
+							await docClient.put({ ...transactionsTable, Item: newTransaction }, err => {
+								if (err) {
+									console.log(err);
+									res.status(500).json({ message: 'An error occured.' });
+									return;
+								}
+								res.status(201).json({ message: 'Transferred!' });
+							});
+						}
+					);
+				}
+			);
+		});
 	});
 
-	app.get('/getUsers', async (req, res, next) => {
-		const cursor = await usersCollection.find().project({ email: 1, id: 1, balance: 1, _id: 0 });
-		var usersObj = [];
+	/*
 
-		cursor
-			.forEach(entry => {
-				usersObj.push(entry);
-			})
-			.then(() => res.status(201).json({ users: usersObj }));
-	});
-
-	app.get('/getAllTransactions', async (req, res, next) => {
-		const cursor = await transactionsCollection.find();
-		var transactions = [];
-
-		cursor
-			.forEach(entry => {
-				transactions.push(entry);
-			})
-			.then(() => {
-				const sortedArray = transactions.sort((a, b) => {
-					return moment(a.timestamp).diff(b.timestamp);
+	await usersCollection
+		.findOneAndUpdate({ id: sender }, { $inc: { balance: -Number(amt) } })
+		.then(async (err, doc) => {
+			await usersCollection
+				.findOneAndUpdate({ id: receiver }, { $inc: { balance: Number(amt) } })
+				.then(async err => {
+					await transactionsCollection
+						.insertOne({
+							sender_id: sender,
+							receiver_id: receiver,
+							amt: Number(amt),
+							timestamp: moment().format(),
+						})
+						.then(() => {
+							res.status(201).json({ message: 'transferred!' });
+						});
 				});
-				sortedArray.forEach(obj => (obj.timestamp = moment(obj.timestamp).fromNow()));
-				res.status(201).json({ transactions });
-			});
-	});
+		});
 
-	app.post('/getTransactionsById', async (req, res, next) => {
-		if (!req.body.id) {
-			res.status(404).json({ message: 'Parameters missing' });
+		*/
+});
+
+app.post('/transact', async (req, res, next) => {
+	//type, amt (debit=0, credit=1)
+	if (!req.body.email || !req.body.type || !req.body.amt) {
+		res.status(400).json({ message: 'Parameters missing' });
+		return;
+	}
+
+	var { email, type, amt } = req.body;
+	amt = Number(amt);
+	var acc_balance, id;
+
+	await docClient.get({ ...usersTable, Key: { email } }, async (err, data) => {
+		if (err) {
+			res.status(500).json({ message: 'An error occured.' });
+			return;
+		}
+		if (Object.keys(data).length === 0) {
+			res.status(404).json({ message: 'User not found.' });
 			return;
 		}
 
-		var transactions = [];
-		var { id } = req.body;
+		acc_balance = data.Item.balance;
+		id = data.Item.id;
 
-		const cursor = await transactionsCollection.find({ $or: [{ sender_id: id }, { receiver_id: id }] });
+		Number(type) === 1 ? credit(acc_balance + amt) : debit(acc_balance - amt);
+	});
 
-		cursor
-			.forEach(entry => {
-				transactions.push(entry);
-			})
-			.then(() => {
-				const sortedArray = transactions.sort((a, b) => {
-					return moment(a.timestamp).diff(b.timestamp);
+	async function debit(amount) {
+		if (acc_balance < amt || acc_balance === 0) {
+			res.status(401).json({ message: 'Insufficient balance.' });
+			return;
+		}
+		await docClient.update(
+			{
+				...usersTable,
+				Key: { email },
+				UpdateExpression: 'set balance = :r',
+				ExpressionAttributeValues: { ':r': amount },
+			},
+			async err => {
+				if (err) {
+					res.status(500).json({ message: 'An error occured.' });
+					return;
+				}
+				const newTransaction = {
+					transaction_id: uuidv4(),
+					sender_id: id,
+					receiver_id: 100,
+					amt: amount,
+					timestamp: moment().format(),
+				};
+				await docClient.put({ ...transactionsTable, Item: newTransaction }, err => {
+					if (err) {
+						console.log(err);
+						res.status(500).json({ message: 'An error occured.' });
+						return;
+					}
+					res.status(201).json({ message: 'Amount debited!' });
 				});
-				sortedArray.forEach(obj => (obj.timestamp = moment(obj.timestamp).fromNow()));
-				res.status(201).json({ sortedArray });
+			}
+		);
+	}
+
+	async function credit(amount) {
+		await docClient.update(
+			{
+				...usersTable,
+				Key: { email },
+				UpdateExpression: 'set balance = :r',
+				ExpressionAttributeValues: { ':r': amount },
+			},
+			async err => {
+				if (err) {
+					res.status(500).json({ message: 'An error occured.' });
+					return;
+				}
+				const newTransaction = {
+					transaction_id: uuidv4(),
+					sender_id: 100,
+					receiver_id: id,
+					amt: amount,
+					timestamp: moment().format(),
+				};
+				await docClient.put({ ...transactionsTable, Item: newTransaction }, err => {
+					if (err) {
+						console.log(err);
+						res.status(500).json({ message: 'An error occured.' });
+						return;
+					}
+					res.status(201).json({ message: 'Amount credited!' });
+				});
+			}
+		);
+	}
+});
+
+app.get('/getUsers', async (req, res, next) => {
+	await docClient.scan({ ...usersTable, ProjectionExpression: 'email, balance, id' }, (err, data) => {
+		if (err) {
+			res.status(500).json({ message: 'An error occured.' });
+			return;
+		}
+		res.status(200).json({ users: data.Items });
+	});
+});
+
+app.get('/getAllTransactions', async (req, res, next) => {
+	var transactions = [];
+	await docClient.scan(transactionsTable, (err, data) => {
+		if (err) {
+			res.status(500).json({ message: 'An error occured.' });
+			return;
+		}
+
+		data.Items.forEach(entry => transactions.push(entry));
+
+		const sortedArray = transactions.sort((a, b) => {
+			return moment(a.timestamp).diff(b.timestamp);
+		});
+		sortedArray.forEach(obj => (obj.timestamp = moment(obj.timestamp).fromNow()));
+
+		res.status(200).json({ transactions });
+	});
+});
+
+app.post('/getTransactionsById', async (req, res, next) => {
+	if (!req.body.id) {
+		res.status(404).json({ message: 'Parameters missing' });
+		return;
+	}
+
+	var transactions = [];
+
+	await docClient.scan(
+		{
+			...transactionsTable,
+			FilterExpression: 'sender_id=:r OR receiver_id=:r',
+			ExpressionAttributeValues: { ':r': req.body.id },
+		},
+		(err, data) => {
+			if (err) {
+				res.status(500).json({ message: 'An error occured.' });
+				return;
+			}
+			if (Object.keys(data).length === 0) {
+				res.status(201).json({ transactions: [] });
+				return;
+			}
+
+			data.Items.forEach(entry => transactions.push(entry));
+
+			const sortedArray = transactions.sort((a, b) => {
+				return moment(a.timestamp).diff(b.timestamp);
 			});
-	});
+			sortedArray.forEach(obj => (obj.timestamp = moment(obj.timestamp).fromNow()));
 
-	app.post('/getBal', async (req, res, next) => {
-		if (!req.body.id) {
-			res.status(401).json({ message: 'Parameters missing' });
+			res.status(200).json({ sortedArray });
+		}
+	);
+});
+
+app.post('/getBal', async (req, res, next) => {
+	if (!req.body.email) {
+		res.status(401).json({ message: 'Parameters missing' });
+		return;
+	}
+
+	await docClient.get({ ...usersTable, Key: { email: req.body.email } }, (err, data) => {
+		if (err) {
+			res.status(500).json({ message: 'An error occured.' });
 			return;
 		}
-		const data = await usersCollection.findOne({ id: req.body.id });
-
-		if (!data) {
-			res.status(404).json({ message: 'No users found' });
+		if (Object.keys(data).length === 0) {
+			res.status(404).json({ message: 'No user found.' });
 			return;
 		}
 
-		res.status(201).json({ balance: data.balance });
+		res.status(200).json({ balance: data.Item.balance });
 	});
+});
 
-	app.listen(port, () => {
-		console.log(`Example app listening at http://localhost:${port}`);
-	});
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
 });
